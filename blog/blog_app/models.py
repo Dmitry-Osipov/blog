1ad81de90ca9a7c5982d51e8b1c0754a00b6092a
Dmitry Osipov <dimaosipov00@gmail.com>
2023-12-10 16:34:25 +0300

    Added a more browser friendly URL. Updated the post model slug so that it is unique only for the date. Updated the paths. Added pagination to the home page. Connected pagination to the main page by attaching a separate HTML file

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
 class PublishedManager(models.Manager):
@@ -30,7 +31,7 @@ class Post(models.Model):
         PUBLISHED = ('PB', 'Published')
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')  # Слаг является уникальным для конкретной даты.
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -49,6 +50,18 @@ class Post(models.Model):
         indexes = (
             models.Index(fields=['-publish']),
         )
+
+    def get_absolute_url(self):
+        """
+        Метод формирует канонический URL-адрес для всех объектов класса.
+
+        :return: Строка перенаправления на определённую страницу.
+        """
+        return reverse('blog_app:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
     def __str__(self):
         """
--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import *
 
 # Create your views here.
@@ -9,11 +10,19 @@ def post_list(request):
     :param request: Запрос клиента.
     :return: HTML-страница со всеми опубликованными постами.
     """
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)  # По умолчанию берём первую страницу.
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     return render(request, 'blog_app/post/list.html', context={'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     """
     Функция представления отвечает за отображение конкретного поста.
 
@@ -22,6 +31,13 @@ def post_detail(request, id):
     :return: HTML-страница опубликованного поста.
     :raises Http404: Ошибка 404, если нет опубликованного поста с переданным id.
     """
-    post = get_object_or_404(Post, pk=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
 
     return render(request, 'blog_app/post/detail.html', context={'post': post})
